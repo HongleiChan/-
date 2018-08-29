12f3e22e89d4c120111d694a573cb728ca7275db
HongleiChan <HongleiChan@163.com>
2018-08-29 15:58:55 +0800

    111

--- src/components/Rsult.vue
+++ src/components/Rsult.vue
@@ -1,30 +1,43 @@
 <template>
-  <div>
+  <div style="margin-top: 300px">
     <h2 align="center">结果显示</h2>
-    <div>
-      <div v-show="chek">
-        <p>文章：</p>
-        <div class="content">
-          {{content}}
-        </div>
+    <div v-show="chek"  >
+      <div class="content">
+        <h3>分句结果：</h3>
+        {{content}}
       </div>
-      <div id="myChart" :style="{width: '60%', height: '600px' ,float: 'left'}"></div>
-      <div style="width: 40%;height: 600px;float: right">
-        <el-table
-          :data="tableData"
-          border>
-          <el-table-column
-            prop="type"
-            label="模型"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="weight"
-            label="结果"
-            width="180">
-          </el-table-column>
-        </el-table>
-       </div>
+    </div>
+    <div>
+      <div class="content">
+        <h3>文档分类结果：</h3>
+        <div id="myChart" :style="{width: '60%', height: '300px' ,float: 'left'}"></div>
+        <div style="width: 40%;height: 300px;float: right">
+          <el-table
+            :data="tableData"
+            border>
+            <el-table-column
+              prop="type"
+              label="模型"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="weight"
+              label="结果"
+              width="180">
+            </el-table-column>
+          </el-table>
+         </div>
+      </div>
+    </div>
+    <div>
+      <div class="content" v-show="chek">
+        <h3>涉黄检测结果：</h3>
+      </div>
+    </div>
+    <div>
+      <div class="content" v-show="chek">
+        <h3>涉赌检测结果：</h3>
+      </div>
     </div>
     <div v-show="chek" align="center" style="padding-top: 20px">
       <el-radio v-model="radio" label="1"  border>cews二分类</el-radio>
@@ -74,7 +87,6 @@
         let myChart = this.$echarts.init(document.getElementById('myChart'));
         // 绘制图表
         myChart.setOption({
-          title: { text: '文档分类结果' },
           tooltip: {},
           xAxis: {
             data: ["时政","房产","科技","教育","游戏","家居","财经","娱乐","时尚","体育"]
@@ -102,11 +114,13 @@
 
 <style>
  .content{
-   width: 100%;
-   height: 300px;
+   margin-right: auto;
+   margin-left: auto;
+   width: 80%;
+   height: 400px;
    border: darkgray solid 1px;
-   overflow: scroll;
+   overflow: hidden;
    overflow-x:hidden;
-   font-size: 18px
+   font-size: 18px;
  }
 </style>
