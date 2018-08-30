8d50821bba05f0c245fb7a644e8a0a1e5a29a354
HongleiChan <HongleiChan@163.com>
2018-08-30 15:10:58 +0800

    11

--- src/components/TextCNN.vue
+++ src/components/TextCNN.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div style="padding-top: 20px">
-      <div style="width: 50%;height: auto;border: black;float: left">
+      <div style="width: 40%;height: auto;border: black;float: left">
         <p style="float:left;">AI平台textCNN:</p>
         <el-button type="primary" style="float:right;margin-right: 50%">点击获取</el-button>
       </div>
-      <div style="width: 50%;height: auto;border: black;float: right">
+      <div style="width: 40%;height: auto;border: black;float: right">
         <p style="float:left;">增加数据集后textCNN:</p>
         <el-button type="primary" style="float:right;margin-right: 50%">点击获取</el-button>
       </div>
